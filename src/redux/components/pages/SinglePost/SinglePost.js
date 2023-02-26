7cd347b333e262d71a3c6b6353af57def0e86b58
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-02-26 19:04:31 +0100

    add modal

--- src/redux/components/pages/SinglePost/SinglePost.js
+++ src/redux/components/pages/SinglePost/SinglePost.js
@@ -3,16 +3,29 @@ import { NavLink, useParams } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import { Button, Card, Col, Row, Container } from 'react-bootstrap';
 import { getPostById } from '../../../postsRedux';
+import { useDispatch } from 'react-redux';
+import { removePost } from '../../../postsRedux';
+import { Modal } from 'react-bootstrap';
+import { useState } from 'react';
 
 const SinglePost = () => {
+  const dispatch = useDispatch();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const { postId } = useParams();
   const postData = useSelector((state) => getPostById(state, postId));
   if (!postData) return <Navigate to='/' />;
+
+  const handleDelete = () => {
+    dispatch(removePost(postId));
+  };
+
   return (
     <>
       <Container>
         <Row>
-          <Col>
+          <Col className='col-6'>
             <Card border='light'>
               <Card.Body>
                 <Card.Title>{postData.title}</Card.Title>
@@ -34,7 +47,26 @@ const SinglePost = () => {
             >
               Edit
             </Button>
-            <Button variant='outline-danger'>Delete</Button>
+            <Button variant='outline-danger' onClick={handleShow}>
+              Delete
+            </Button>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Are You Sure?</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                This operation will pernamently and irreversibly delete this
+                post
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant='secondary' onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant='outline-danger' onClick={handleDelete}>
+                  Delete
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </Col>
         </Row>
       </Container>
